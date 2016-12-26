41a080e4e2aa7f37cef0e93ba33aea67f36be91e
Krishna murthy <krishna.murthy@coupondunia.in>
2016-12-26 21:59:36 +0530

    tour files

---
+++ add.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+    return x + y
+}
+
+func main() {
+//    fmt.Println(add(42,43))
+    // Another way for add
+    sum := func(a, b int) int { return a + b } (3, 4)
+    fmt.Println(sum)
+}
