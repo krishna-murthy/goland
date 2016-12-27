175c2a6a3e68d61624989acc1d2837c76187d2a9
Krishna murthy <krishna.murthy@coupondunia.in>
2016-12-27 11:19:28 +0530

    more tour basics

---
+++ constants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const PI = 3.14
+
+func main() {
+    const world = "India"
+    fmt.Println("Hello", world)
+    fmt.Println("Happy", PI, "day")
+    const Truth = true
+    fmt.Println("Go Rules?", Truth)
+}
