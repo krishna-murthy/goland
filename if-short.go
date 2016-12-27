0aa01a58327d5fd02b5693669396f1f7d8b43bae
Krishna murthy <krishna.murthy@coupondunia.in>
2016-12-27 12:22:29 +0530

    format and control structures

---
+++ if-short.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func limit(x, y, lim float64) float64 {
+	if v := math.Pow(x, y); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(limit(3, 2, 10))
+	fmt.Println(limit(3, 3, 20))
+}
