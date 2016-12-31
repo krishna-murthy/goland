f6dc9da2c7d60129107d8087633127af7c4f006f
Krishna murthy <krishna.murthy@coupondunia.in>
2016-12-31 19:26:52 +0530

    functions done

--- function-values.go
+++ function-values.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
-func compute(fn func(float64, float64)float64) float64 {
-    return fn(3, 4)
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
 }
 
 func main() {
-    hyplot := func(x, y float64) float64 {
-        return math.Sqrt( x*x + y*y )
-    }
+	hyplot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
 
-    fmt.Println(hyplot(5, 12))
+	fmt.Println(hyplot(5, 12))
 
-    fmt.Println(compute(hyplot))
-    fmt.Println(compute(math.Pow))
+	fmt.Println(compute(hyplot))
+	fmt.Println(compute(math.Pow))
 }
