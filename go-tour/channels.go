10ae6a4133aebb232b1da4d2575b6cc0e9e518d5
krishnamurthy <krishhthecool@gmail.com>
2017-03-14 21:19:04 +0530

    concurrency

---
+++ go-tour/channels.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func main() {
+	a := []int{3, 2, 22, 1, 3, 4}
+	c := make(chan int)
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+	x, y := <-c, <-c
+	fmt.Println(x, y, x+y)
+}
--- go-tour/clock.go
+++ go-tour/clock.go
@@ -5,9 +5,10 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
+
 // The value of testVersion here must match `targetTestVersion` in the file
 // clock_test.go.
 const testVersion = 4
@@ -16,41 +17,41 @@ const testVersion = 4
 // More details and hints are in clock_test.go.
 
 type Clock struct {
-    t time.Time
+	t time.Time
 }
 
 func New(hour, minute int) Clock {
-    var r Clock
-    t := time.Date(1988, time.January, 1, hour, 0, 0, 0, time.UTC)
-    m := time.Duration(minute) * time.Minute
-    s := t.Add(m)
-    r.t = s
-    return r
+	var r Clock
+	t := time.Date(1988, time.January, 1, hour, 0, 0, 0, time.UTC)
+	m := time.Duration(minute) * time.Minute
+	s := t.Add(m)
+	r.t = s
+	return r
 }
 
 func (c Clock) String() string {
-    t := c.t.Minute()
-    s := c.t.Hour()
-    return fmt.Sprintf("%02d:%02d\n", s, t)
+	t := c.t.Minute()
+	s := c.t.Hour()
+	return fmt.Sprintf("%02d:%02d\n", s, t)
 }
 
 func (c Clock) Add(minutes int) Clock {
-    t := c.t
-    m := time.Duration(minutes) * time.Minute
-    s := t.Add(m)
-    c.t = s
-    return c
+	t := c.t
+	m := time.Duration(minutes) * time.Minute
+	s := t.Add(m)
+	c.t = s
+	return c
 }
 
 func main() {
-    var g Clock
-    g = New(7, 32)
-    t := g.t
-    fmt.Println(t.Day())
-    var c Clock
-    c = New(-12, -268)
-    t = c.t
-    fmt.Println(t.Day())
-    fmt.Println(g == c)
-    fmt.Println(g.String())
+	var g Clock
+	g = New(7, 32)
+	t := g.t
+	fmt.Println(t.Day())
+	var c Clock
+	c = New(-12, -268)
+	t = c.t
+	fmt.Println(t.Day())
+	fmt.Println(g == c)
+	fmt.Println(g.String())
 }
